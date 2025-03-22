65b6c1563687d0ae6be3529e87228fc3b9a79ef3
Telmo Carmo <telmo.carmo@gmail.com>
2025-03-22 20:03:04 +0000

    JWT generation is Not working

--- src/main/java/pt/nb/dsi/AuthResource.java
+++ src/main/java/pt/nb/dsi/AuthResource.java
@@ -1,5 +1,10 @@
 package pt.nb.dsi;
 
+/*
+ smallrye.jwt.sign.key.location="my-secret-symmetric-key in 32 BY"
+ smallrye.jwt.sign.key.location="privateKey.pem"
+ */
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,23 +16,59 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 
+import io.quarkus.security.UnauthorizedException;
+import io.smallrye.jwt.build.Jwt;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+
 import io.quarkus.logging.Log;
 
 @Path("/auth")
 public class AuthResource {
 
-
+    @ConfigProperty(name = "jwt.duration", defaultValue = "3600") // 1 hour default
+    long jwt_duration;
 
     @POST
     @Path("/login")
+    //@PermitAll    //import javax.annotation.security.PermitAll;
     @Produces(MediaType.APPLICATION_JSON)
-    public Map<String,String> login() {
+    public Map<String,String> login(LoginReq req) {
+        Log.infof("AuthResource.login %s %s",req.uid,req.pwd);
+        String token = null;
+
+        if (authenticate(req.uid, req.pwd)) {
+            
+            token = Jwt.
+                claims()
+                    .subject(req.uid)
+                    .claim("roles", "USER")
+                    .claim("exp", System.currentTimeMillis() / 1000 + jwt_duration)
+                    .claim("aud", "https://wwwcert.bportugal.pt/adfs/oauth2/token")
+                .sign();
+
+
+            /* 
+            Set<String> roles = new HashSet<>();
+            roles.add("User"); // or admin, etc.
+
+            token = Jwt.issuer("quarkus-jwt")
+                    .subject(req.uid)
+                    .upn(req.uid)
+                    .groups(roles)
+                    .expiresIn(Duration.ofSeconds(jwt_duration))
+                    .sign();
+            */
+        } else {
+            Log.error("Invalid login credentials");
+            throw new UnauthorizedException("Invalid credentials");
+        }
         Map<String,String> map1 = new HashMap<>();
-        Log.info("AuthResource.login");
+
         map1.put("username", "demo");
-        map1.put("token", 
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwic3ViIjoiZGVtbyIsInJvbGVzIjoiVVNFUiIsImV4cCI6MTc0MjY5NDMwMywiYXVkIjoiaHR0cHM6Ly93d3djZXJ0LmJwb3J0dWdhbC5uZXQvYWRmcy9vYXV0aDIvdG9rZW4ifQ.GT28Ai8n7u_UD7wTcdmXxK8kIDzjNXg-LcBXCkCQ-lg");
+        map1.put("token",  "Bearer " + token);
         map1.put("error", null);
+        Log.infof("AuthResource.login JWT token=%s",token);
         return map1;
     }
     
@@ -44,5 +85,17 @@ public class AuthResource {
         double rn = Math.random();
         Log.warnf("/auth Random = %f",rn);
         return rn;
+    }
+
+    private boolean authenticate(String username, String password) {
+        // Replace with your database or external authentication system
+        // Example (very insecure):
+        return  "123".equals(password);
+    }
+
+    public static class LoginReq {
+        public String uid;
+        public String pwd;
+        public String aut;
     }
 }
