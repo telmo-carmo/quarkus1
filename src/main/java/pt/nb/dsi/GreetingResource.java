efac735feb48f93393337f75fa66b5886aac897b
Telmo Carmo <telmo.carmo@gmail.com>
2025-03-22 14:08:13 +0000

    added bonus repo

--- src/main/java/pt/nb/dsi/GreetingResource.java
+++ src/main/java/pt/nb/dsi/GreetingResource.java
@@ -1,21 +1,55 @@
 package pt.nb.dsi;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import javax.sql.DataSource;
+
+import io.quarkus.logging.Log;
+
 
 @Path("/hello")
 public class GreetingResource {
+
+    @Inject
+    DataSource dataSource;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         Date now = new Date();
-        String isoNow = java.time.format.DateTimeFormatter.ISO_INSTANT
-            .format(now.toInstant());
+        String isoNow = java.time.format.DateTimeFormatter.ISO_INSTANT.format(now.toInstant());
+        Log.warn("Hello at " + isoNow);
         return "Hello from Quarkus REST \r\nat " + isoNow;
+    }
+
+    @GET
+    @Path("/up")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, String> upSatus() {
+        Map<String, String> res = new HashMap<>();
+        res.put("status", "up");
+
+        try (Connection connection = dataSource.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT version();")) {
+            if (resultSet.next()) {
+                res.put("dbver", resultSet.getString(1));
+            }
+        } catch (Exception e) {
+            res.put("dbver", "unknown");
+            e.printStackTrace();
+        }
+
+        return res;
     }
 }
--- src/main/java/pt/nb/dsi/dal/BonusRepository.java
+++ src/main/java/pt/nb/dsi/dal/BonusRepository.java
@@ -1,15 +1,150 @@
 package pt.nb.dsi.dal;
 
+import jakarta.inject.Inject;
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
-public interface BonusRepository {
-    public int          count();
-    public List<Bonus>  findAll();
-    public Bonus        findOne(String ename);
-    public Bonus        save(Bonus bn);
-    public boolean      update(Bonus b);
-    public boolean      delete(String ename);
-    
-    public List<Bonus>  findRange(int from, int to);
-}
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+import io.quarkus.logging.Log;
+
+@RegisterForReflection
+public class BonusRepository implements IBonusRepository {
+
+    @Inject
+    DataSource dataSource;
+
+    @Override
+    public int count() {
+        String query = "SELECT COUNT(*) FROM Bonus";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query);
+             ResultSet resultSet = statement.executeQuery()) {
+            if (resultSet.next()) {
+                return resultSet.getInt(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    @Override
+    public List<Bonus> findAll() {
+        List<Bonus> bonuses = new ArrayList<>();
+        String query = "SELECT * FROM Bonus";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query);
+             ResultSet resultSet = statement.executeQuery()) {
+            while (resultSet.next()) {
+                bonuses.add(mapRowToBonus(resultSet));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        Log.infof("Number of bonuses retrieved: %d", bonuses.size());
+        return bonuses;
+    }
+
+    @Override
+    public Bonus findOne(String ename) {
+        String query = "SELECT * FROM Bonus WHERE ename = ?";
+        Log.warnf("Get bonus for %s",ename);
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, ename);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return mapRowToBonus(resultSet);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public Bonus save(Bonus bn) {
+        String query = "INSERT INTO Bonus (ename,job,sal,comm) VALUES (?,?,?,?)";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, bn.getJob());
+            statement.setString(2, bn.getEname());
+            statement.setInt(3, bn.getSal());
+            statement.setInt(4, bn.getComm());
+            int affectedRows = statement.executeUpdate();
+            if (affectedRows > 0) {
+
+                        return bn;
+                
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public boolean update(Bonus b) {
+        String query = "UPDATE Bonus SET job = ?, sal = ? , comm = ?  WHERE ename = ?";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+                statement.setString(4, b.getEname());
+                statement.setString(1, b.getJob());
+                statement.setInt(2, b.getSal());
+                statement.setInt(3, b.getComm());
+            return statement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean delete(String ename) {
+        String query = "DELETE FROM Bonus WHERE ename = ?";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, ename);
+            return statement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public List<Bonus> findRange(int from, int to) {
+        List<Bonus> bonuses = new ArrayList<>();
+        String query = "SELECT * FROM Bonus LIMIT ? OFFSET ?";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, to - from + 1);
+            statement.setInt(2, from);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    bonuses.add(mapRowToBonus(resultSet));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return bonuses;
+    }
+
+    private Bonus mapRowToBonus(ResultSet resultSet) throws SQLException {
+        Bonus bonus = new Bonus();
+        bonus.setEname(resultSet.getString("ename"));
+        bonus.setJob(resultSet.getString("job"));
+        bonus.setSal(resultSet.getInt("sal"));
+        bonus.setComm(resultSet.getInt("comm"));
+        return bonus;
+    }
+}
