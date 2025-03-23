1b7baf09da6e67c79c03188badcc157d6b4a43cd
Telmo Carmo <telmo.carmo@gmail.com>
2025-03-23 15:58:41 +0000

    correted sh scripts

--- start1.sh
+++ start1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 nohup java -jar target/quarkus1-1.0.0-SNAPSHOT-runner.jar > quarkus1.log  2>&1  &
-echo PID = $! > quarkus1.pid
+echo $! > quarkus1.pid
 # http://10.221.132.29:8080/swagger-ui/index.html
 
 if [ -f quarkus1.pid ]; then
