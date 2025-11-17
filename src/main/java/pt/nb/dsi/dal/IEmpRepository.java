117289a620d3917a61b9fef1178bb0c5f465453f
Telmo Carmo <telmo.carmo@gmail.com>
2025-11-17 17:51:26 +0000

    changed javax pkgs  to jakarta

--- src/main/java/pt/nb/dsi/dal/IEmpRepository.java
+++ src/main/java/pt/nb/dsi/dal/IEmpRepository.java
@@ -1,7 +1,7 @@
 package pt.nb.dsi.dal;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import javax.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public interface IEmpRepository extends PanacheRepository<Emp> {
